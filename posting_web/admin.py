e8157e635ad174b4ac3f7b15f939686dcc1ba676
Danil <105702087+Danisimo0@users.noreply.github.com>
2023-06-26 02:32:04 +0600

    up

--- posting_web/admin.py
+++ posting_web/admin.py
@@ -1,3 +1,18 @@
-from django.contrib import admin
 
-# Register your models here.
+
+from django.contrib import admin
+from .models import Item, Profile, Product, Rating, Comment
+
+
+admin.site.register(Item)
+
+
+admin.site.register(Profile)
+
+
+admin.site.register(Product)
+
+
+admin.site.register(Rating)
+
+admin.site.register(Comment)
